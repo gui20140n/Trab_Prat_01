d0234794e1aafbdd25d451f53b3e040ca1cb296a
João Guilherme <gui2015on@outlook.com.br>
2021-05-13 15:00:55 -0300

    testando again

---
+++ N_Classe.java
@@ -0,0 +1,34 @@
+// testando
+
+/**
+ * Escreva uma descrição da classe N_Classe aqui.
+ * 
+ * @author João Guilherme - 474041 - Estatística_UFC
+ * @version (um número da versão ou uma data)
+ */
+public class N_Classe
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private int x;
+
+    /**
+     * Construtor para objetos da classe N_Classe
+     */
+    public N_Classe()
+    {
+        // inicializa variáveis de instância
+        x = 0;
+    }
+
+    /**
+     * Um exemplo de um método - substitua este comentário pelo seu próprio
+     * 
+     * @param  y   um exemplo de um parâmetro de método
+     * @return     a soma de x e y 
+     */
+    public int sampleMethod(int y)
+    {
+        // escreva seu código aqui
+        return x + y;
+    }
+}
